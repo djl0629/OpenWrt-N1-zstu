95b6b8bf131cf7073b9a0299cd57535a7cf464cf
nANAKo <33384253+djl0629@users.noreply.github.com>
2021-12-06 23:53:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 
 git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
 git clone https://github.com/tuanqing/install-program package/install-program
+git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
